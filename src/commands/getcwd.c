0572026a926e273c872486b5b9f3eead692fa273
George Gilmartin <gmgq72@mail.missouri.edu>
2015-04-27 21:38:39 -0500

    Get directory setup

--- src/commands/getcwd.c
+++ src/commands/getcwd.c
@@ -1,1 +1,21 @@
-TEST
+#include "../../include/commands.h"
+//#include <stdlib.h>
+//#include <unistd.h>
+
+
+char* getDirectory{
+	
+	long size;
+	char *buf;
+	char *ptr;
+
+	size = pathconf(".", _PC_PATH_MAX);
+
+	if ((buf = (char *)malloc((size_t)size)) != NULL){
+		ptr = getcwd(buf, (size_t)size);
+	}
+	else{
+		perror("\nCould not get working directory!\n");
+	}
+	return ptr;
+}
